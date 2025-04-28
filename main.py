0b588755d90f18e18663e80015523ec736bff4ba
Gsr1989 <empresalorens@gmail.com>
2025-04-27 21:53:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -102,10 +102,10 @@ def formulario():
         page.insert_text((400, 1700), f"NÚMERO DE SERIE: {serie}", fontsize=35)
         page.insert_text((375, 1745), f"NÚMERO DE MOTOR: {motor}", fontsize=35)
         page.insert_text((602, 1790), f"MARCA: {marca}", fontsize=35)
-        page.insert_text((580, 1835), f"MODELO: {linea}", fontsize=35)
-        page.insert_text((650, 1880), f"AÑO: {año}", fontsize=35)
-        page.insert_text((670, 1925), f"COLOR: {color}", fontsize=35)
-        page.insert_text((420, 1970), f"CONTRIBUYENTE: {contribuyente}", fontsize=35)
+        page.insert_text((575, 1835), f"MODELO: {linea}", fontsize=35)
+        page.insert_text((653, 1880), f"AÑO: {año}", fontsize=35)
+        page.insert_text((600, 1925), f"COLOR: {color}", fontsize=35)
+        page.insert_text((425, 1970), f"CONTRIBUYENTE: {contribuyente}", fontsize=35)
 
         # Guardar PDF
         if not os.path.exists(PDF_OUTPUT_FOLDER):
