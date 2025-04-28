afc25c738799bff55544b656eebe7caeef339d93
Gsr1989 <empresalorens@gmail.com>
2025-04-27 21:35:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -100,9 +100,9 @@ def formulario():
         page.insert_text((247, 1550), f"CARACTERÍSTICAS {tipo_texto}:", fontsize=80)
 
         page.insert_text((400, 1700), f"NÚMERO DE SERIE: {serie}", fontsize=35)
-        page.insert_text((380, 1745), f"NÚMERO DE MOTOR: {motor}", fontsize=35)
+        page.insert_text((375, 1745), f"NÚMERO DE MOTOR: {motor}", fontsize=35)
         page.insert_text((600, 1790), f"MARCA: {marca}", fontsize=35)
-        page.insert_text((600, 1835), f"MODELO: {linea}", fontsize=35)
+        page.insert_text((550, 1835), f"MODELO: {linea}", fontsize=35)
         page.insert_text((460, 1880), f"AÑO: {año}", fontsize=35)
         page.insert_text((460, 1925), f"COLOR: {color}", fontsize=35)
         page.insert_text((470, 1970), f"CONTRIBUYENTE: {contribuyente}", fontsize=35)
