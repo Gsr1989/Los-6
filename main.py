a1ddbb5e42312dfcdbbe18ad0fcb55d8ed61b5c3
Gsr1989 <empresalorens@gmail.com>
2025-05-02 01:21:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -62,20 +62,19 @@ def generar_pdf(folio, marca, linea, año, serie, motor, color, contribuyente, f
     page.insert_text((376, 729), motor, fontsize=8)
     page.insert_text((376, 700), marca, fontsize=8)
     page.insert_text((376, 714), linea, fontsize=8)
-    page.insert_text((376, 441), año, fontsize=8)
     page.insert_text((376, 756), color, fontsize=8)
     page.insert_text((122, 700), contribuyente, fontsize=8)
 
     # PARTE INFERIOR ROTADA 90° A LA IZQUIERDA
-    page.insert_text((440, 200), folio, fontsize=24, rotate=270, color=(0, 0, 0))
+    page.insert_text((440, 200), folio, fontsize=83, rotate=270, color=(0, 0, 0))
     page.insert_text((500, 670), fecha_expedicion, fontsize=19, rotate=270, color=(0, 0, 0))
     page.insert_text((500, 690), fecha_vencimiento, fontsize=19, rotate=270, color=(0, 0, 0))
-    page.insert_text((500, 100), serie, fontsize=19, rotate=270, color=(0, 0, 0))
-    page.insert_text((220, 120), motor, fontsize=19, rotate=270, color=(0, 0, 0))
-    page.insert_text((280, 120), marca, fontsize=19, rotate=270, color=(0, 0, 0))
-    page.insert_text((280, 200), linea, fontsize=19, rotate=270, color=(0, 0, 0))
-    page.insert_text((280, 260), año, fontsize=19, rotate=270, color=(0, 0, 0))
-    page.insert_text((500, 240), color, fontsize=19, rotate=270, color=(0, 0, 0))
+    page.insert_text((168, 115), serie, fontsize=19, rotate=270, color=(0, 0, 0))
+    page.insert_text((224, 115), motor, fontsize=19, rotate=270, color=(0, 0, 0))
+    page.insert_text((280, 115), marca, fontsize=19, rotate=270, color=(0, 0, 0))
+    page.insert_text((280, 180), linea, fontsize=19, rotate=270, color=(0, 0, 0))
+    page.insert_text((280, 300), año, fontsize=19, rotate=270, color=(0, 0, 0))
+    page.insert_text((224, 250), color, fontsize=19, rotate=270, color=(0, 0, 0))
     page.insert_text((500, 830), contribuyente, fontsize=19, rotate=270, color=(0, 0, 0))
 
     if not os.path.exists(PDF_OUTPUT_FOLDER):
