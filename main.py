f83c4346022205656a8d94e839e8d77d75833991
Gsr1989 <empresalorens@gmail.com>
2025-05-23 23:05:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -199,5 +199,10 @@ def reimprimir():
             flash("No se encontró el PDF con ese folio", "danger")
     return render_template('reimprimir.html')
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
 if __name__ == '__main__':
     app.run(debug=True)
